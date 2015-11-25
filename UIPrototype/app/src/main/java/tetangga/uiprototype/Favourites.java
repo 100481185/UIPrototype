19de3bb3aada86fdd8d5965a0bcdafa92d40ccf8
unknown <100481185@UOSL15-R902678P.oncampus.local>
2015-11-25 09:09:08 -0500

    Repository created

---
+++ UIPrototype/app/src/main/java/tetangga/uiprototype/Favourites.java
@@ -0,0 +1,96 @@
+package tetangga.uiprototype;
+
+import android.app.Activity;
+import android.app.ActivityGroup;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.Gravity;
+
+public class Favourites extends Activity {
+
+    private static LinearLayout routeB;
+    private static View.OnClickListener eve;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites);
+
+        LinearLayout routeA = (LinearLayout)findViewById(R.id.routeA);
+        routeA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView stop = (TextView) findViewById(R.id.busStops1);
+                String ro = stop.getText().toString();
+                String[] address = ro.split(" -> ");
+
+                MainActivity.current = address[0];
+                MainActivity.destination = address[1];
+                MainActivity.startTime = getResources().getStringArray(R.array.A_start);
+                MainActivity.endTime = getResources().getStringArray(R.array.A_end);
+
+                MainActivity.startInt = getResources().getString(R.string.startInt);
+                MainActivity.endInt = getResources().getString(R.string.endInt_A);
+                MainActivity.note = getResources().getString(R.string.notifA);
+
+                Maps.updated = true;
+                Schedule.updated = true;
+
+                MainActivity.switchTab(2);
+            }
+        });
+
+        routeB = (LinearLayout)findViewById(R.id.routeB);
+        eve = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView stop = (TextView) findViewById(R.id.busStops2);
+                String ro = stop.getText().toString();
+                String[] address = ro.split(" -> ");
+
+                MainActivity.current = address[0];
+                MainActivity.destination = address[1];
+                MainActivity.startTime = getResources().getStringArray(R.array.B_start);
+                MainActivity.endTime = getResources().getStringArray(R.array.B_end);
+
+                MainActivity.startInt = getResources().getString(R.string.startInt);
+                MainActivity.endInt = getResources().getString(R.string.endInt_B);
+                MainActivity.note = getResources().getString(R.string.notifB);
+
+                Maps.updated = true;
+                Schedule.updated = true;
+
+                MainActivity.switchTab(2);
+            }
+        };
+        disableB();
+
+        Button favRemove = (Button) findViewById(R.id.favBtnRemove2);
+
+        View.OnClickListener btnEve = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                disableB();
+            }
+        };
+        favRemove.setOnClickListener(btnEve);
+    }
+
+    public static void disableB() {
+        routeB.setVisibility(View.INVISIBLE);
+        routeB.setOnClickListener(null);
+    }
+
+    public static void enableB() {
+        routeB.setVisibility(View.VISIBLE);
+        routeB.setOnClickListener(eve);
+    }
+}
---
+++ UIPrototype/app/src/main/java/tetangga/uiprototype/Schedule.java
@@ -0,0 +1,144 @@
+package tetangga.uiprototype;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class Schedule extends Activity
+{
+    public static boolean updated;
+    private LinearLayout timeList;
+    private LinearLayout.LayoutParams listParam;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        this.onResume();
+
+        Spinner spinCompany = (Spinner) findViewById(R.id.schCompanySel);
+        ArrayAdapter<String> adpCompany = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, MainActivity.routeCom);
+        spinCompany.setAdapter(adpCompany);
+        spinCompany.setSelection(0);
+
+        Spinner spinRoute = (Spinner) findViewById(R.id.schRouteSel);
+        ArrayAdapter<String> adpRoute = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, MainActivity.routeNum);
+        spinRoute.setAdapter(adpRoute);
+        spinRoute.setSelection(0);
+
+        Button favButton = (Button) findViewById(R.id.schBtnFav);
+        View.OnClickListener favEve = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Favourites.enableB();
+                MainActivity.switchTab(0);
+            }
+        };
+        favButton.setOnClickListener(favEve);
+
+        Button timeButton = (Button) findViewById(R.id.schBtnTime);
+        View.OnClickListener timeEve = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePicker;
+                datePicker = new DatePickerDialog(Schedule.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectedyear, int selectedmonth, int selectedday) {
+                        selectedmonth++;
+                        System.out.println("Date: " + selectedday + ", " + selectedmonth + "/" + selectedyear);
+                    }
+                }, year, month, day);
+                datePicker.setTitle("Select the Date");
+                datePicker.show();
+            }
+        };
+        timeButton.setOnClickListener(timeEve);
+
+        ScrollView timeListScroll = (ScrollView) findViewById(R.id.schTimeList);
+        ScrollView.LayoutParams scrollParam = new ScrollView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        timeList = new LinearLayout(this);
+        timeList.setLayoutParams(scrollParam);
+        timeList.setOrientation(LinearLayout.VERTICAL);
+        listParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1.0f);
+
+        timeListScroll.addView(timeList);
+    }
+
+    private LinearLayout getView(String left, String right, LinearLayout.LayoutParams param)
+    {
+        LinearLayout lay = new LinearLayout(this);
+        lay.setOrientation(LinearLayout.HORIZONTAL);
+        lay.setLayoutParams(param);
+
+        LinearLayout.LayoutParams textParam = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 0.5f);
+
+        TextView le = new TextView(this);
+        TextView ri = new TextView(this);
+        le.setText(left);
+        ri.setText(right);
+        le.setLayoutParams(textParam);
+        ri.setLayoutParams(textParam);
+        le.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        ri.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        le.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15);
+        ri.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15);
+        lay.addView(le);
+        lay.addView(ri);
+
+        return lay;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        if(this.updated) {
+            this.updated = false;
+            if(timeList != null) timeList.removeAllViews();
+            EditText from = (EditText)findViewById(R.id.schFromEnter);
+            from.setText(MainActivity.current);
+
+            EditText to = (EditText)findViewById(R.id.schToEnter);
+            to.setText(MainActivity.destination);
+
+            for(int t = 0; t < MainActivity.startTime.length; t++){
+                timeList.addView(getView(MainActivity.startTime[t], MainActivity.endTime[t], listParam));
+            }
+
+            TextView intF = (TextView)findViewById(R.id.schTextFrom);
+            intF.setText(MainActivity.startInt);
+
+            TextView intT = (TextView)findViewById(R.id.schTextTo);
+            intT.setText(MainActivity.endInt);
+
+            TextView alert = (TextView)findViewById(R.id.textView3);
+            alert.setText(MainActivity.note);
+        }
+    }
+}
+
